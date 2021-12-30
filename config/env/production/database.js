51c3094ad6241385dd6d6ad8c971bce2ee55faf7
GjLongo <92173095+GjLongo@users.noreply.github.com>
2021-12-30 10:21:46 +0000

    DATABASE_URL var added

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,5 +1,5 @@
 const parse = require('pg-connection-string').parse;
-    const config = parse(process.env.postgres://arilzsteffkupm:eba2de5dc6b477b2b3689f1bec08b6741646b34f9314ac30babe83bd2d85104f@ec2-79-125-30-28.eu-west-1.compute.amazonaws.com:5432/dbdqacc0tlt4r4);
+    const config = parse(process.env.DATABASE_URL);
     module.exports = ({ env }) => ({
       connection: {
         client: 'postgres',
